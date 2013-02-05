60e356da61d61abcf85710440aaababa00b9ff83
Greg Maza <mazag52@gmail.com>
2013-02-05 11:46:43 -0600

    rails original folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Habitpow::Application.config.secret_token = 'c440749a9c81af1ff6b5331c82c279b429f35d67a3bc6d0e9ea5c863150b61d9526e8cb4fc14c15e465c84c24bd57fd4b1d393aa843310fbec9dead39afeed2e'
